f9ac251b2cab03ef031fee7d2d1706644b2710ff
Muhammad Yousef Mirza <142999350+ym2806@users.noreply.github.com>
2025-02-27 22:23:00 +0100

    Removed unwanted files for .gitignore

--- server/models/User.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const userSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
-  email: { type: String, required: true, unique: true },
-  role: { type: String, enum: ["anonymous", "registered", "verified"], default: "anonymous" }, // User role
-});
-
-export const User = mongoose.model("User", userSchema);
